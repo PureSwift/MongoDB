b900bd2db395e172486bade33a70d02499db8eba
Alsey Coleman Miller <alseycmiller@gmail.com>
2015-12-25 12:10:16 -0500

    added test cases for Cursor

--- Sources/UnitTests/CursorTests.swift
+++ Sources/UnitTests/CursorTests.swift
@@ -29,9 +29,76 @@ class CursorTests: XCTestCase {
     
     #endif
 
-    func testExample() {
+    func testProperties() {
         
+        // insert document
         
+        let databaseName = "Test\(Int(Date().timeIntervalSince1970))"
+        
+        let client = MongoDB.Client(URL: "mongodb://localhost:27017")
+        
+        let collection = MongoDB.Collection("\(__FUNCTION__)", database: databaseName, client: client)
+        
+        var document = BSON.Document()
+        
+        document["_id"] = .ObjectID(BSON.ObjectID())
+        
+        document["hello"] = .String("world")
+        
+        do { try collection.insert(document) }
+            
+        catch { XCTFail("\(error)"); return }
+        
+        // create cursor
+        
+        guard let cursor = collection.find(document)
+            else { XCTFail("Could not create cursor"); return }
+        
+        print("Cursor: \(cursor)")
+        
+        XCTAssert(cursor.host.isEmpty == false)
+        
+        XCTAssert(cursor.isAlive)
+        
+        XCTAssert(cursor.more)
+    }
+    
+    func testCopying() {
+        
+        // insert document
+        
+        let databaseName = "Test\(Int(Date().timeIntervalSince1970))"
+        
+        let client = MongoDB.Client(URL: "mongodb://localhost:27017")
+        
+        let collection = MongoDB.Collection("\(__FUNCTION__)", database: databaseName, client: client)
+        
+        var document = BSON.Document()
+        
+        document["_id"] = .ObjectID(BSON.ObjectID())
+        
+        document["hello"] = .String("world")
+        
+        do { try collection.insert(document) }
+            
+        catch { XCTFail("\(error)"); return }
+        
+        // create cursor
+        
+        guard let cursor = collection.find(document)
+            else { XCTFail("Could not create cursor"); return }
+        
+        let resultArray = Array<BSON.Document>(GeneratorSequence(cursor.generator))
+        
+        // create copy
+        
+        let copy = cursor.copy
+        
+        let copyResultArray = Array<BSON.Document>(GeneratorSequence(copy.generator))
+        
+        XCTAssert(resultArray.first! == copyResultArray.first!)
+        XCTAssert(resultArray.count == 1)
+        XCTAssert(copyResultArray.count == 1)
     }
 
 }
